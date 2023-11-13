48e48acd594b52b32d5b7d93d0623063de1a4e60
gmateusz92 <gmateusz92@gmail.com>
2023-11-13 22:00:12 +0100

    1

--- halls/views.py
+++ halls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# def home(request):
+#     recent_halls = Hall.objects.all().order_by('-id')[:3]
+#     popular_halls = [Hall.objects.get(pk=1),Hall.objects.get(pk=2),Hall.objects.get(pk=3)]
+#     return render(request, 'halls/home.html', {'recent_halls':recent_halls, 'popular_halls':popular_halls})
+
+def home(request):
+    return render(request, 'halls/home.html')
