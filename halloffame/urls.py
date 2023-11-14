28e724d290ad13e74832105e40b2cce625752c6f
gmateusz92 <gmateusz92@gmail.com>
2023-11-14 14:49:58 +0100

    1

--- halloffame/urls.py
+++ halloffame/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from halls import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
