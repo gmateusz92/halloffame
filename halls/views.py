e6b3c73f08e27edeecab7d521daaf6038b194750
gmateusz92 <gmateusz92@gmail.com>
2023-11-14 21:40:24 +0100

    1

--- halls/views.py
+++ halls/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from .models import Hall
+
 
 # Create your views here.
 # def home(request):
@@ -8,3 +13,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'halls/home.html')
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+
+    # def form_valid(self, form):
+    #     view = super(SignUp, self).form_valid(form)
+    #     username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+    #     user = authenticate(username=username, password=password)
+    #     login(self.request, user)
+    #     return view
+
+class CreateHall(LoginRequiredMixin, generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
