0d489f9495125972cdc292e9484dc79365bdce89
gmateusz92 <gmateusz92@gmail.com>
2023-11-17 21:28:01 +0100

    1

--- halls/views.py
+++ halls/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Hall
 
 
@@ -14,20 +17,47 @@ from .models import Hall
 def home(request):
     return render(request, 'halls/home.html')
 
+def dashboard(request):
+    halls = Hall.objects.filter(user=request.user)
+    return render(request, 'halls/dashboard.html', {'halls':halls})
+
+
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
-    # def form_valid(self, form):
-    #     view = super(SignUp, self).form_valid(form)
-    #     username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
-    #     user = authenticate(username=username, password=password)
-    #     login(self.request, user)
-    #     return view
+    def form_valid(self, form): # dzieki temu automatyczne logowanie
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
 
-class CreateHall(LoginRequiredMixin, generic.CreateView):
+
+
+class CreateHall(generic.CreateView): #generic views to gdy uzytkownik nie musi byc zalogowany
     model = Hall
     fields = ['title']
     template_name = 'halls/create_hall.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('dashboard')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
+    
+class DetailHall(generic.DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'    
+
+class UpdateHall(generic.UpdateView):
+    model = Hall
+    fields = ['title']  # Dodaj pola, które chcesz zaktualizować
+    template_name = 'halls/update_hall.html'  # Dodaj odpowiedni szablon
+    success_url = reverse_lazy('dashboard')     
+
+class DeleteHall(generic.DeleteView):
+    model = Hall
+    template_name = 'halls/delete_hall.html'  # Dodaj odpowiedni szablon
+    success_url = reverse_lazy('dashboard')      
